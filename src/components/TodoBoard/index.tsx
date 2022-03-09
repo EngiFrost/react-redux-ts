d7533d300dcc4c3ab1f2a71e97a0cfd980b3d9bf
Daniel Frost <flint.bets@yandex.ru>
2022-03-09 21:33:48 +0300

    Moved form to separate component

--- src/components/TodoBoard/index.tsx
+++ src/components/TodoBoard/index.tsx
@@ -1,6 +1,6 @@
-import { Button, TextField } from '@mui/material';
 import React, { useState } from 'react';
 import { Todo } from '../../models/Todo';
+import AddTodoForm from '../AddTodoForm';
 import Flex from '../layout/Flex';
 import Text from '../layout/Text';
 import TodoCard from '../TodoCard';
@@ -10,24 +10,18 @@ const TodoBoard: React.FC = (props) => {
   const styles = todoBoardStyles;
 
   const [todos, setTodos] = useState<Todo[]>([]);
-  const [value, setValue] = useState('');
 
-  const handleChange = (event: { target: { value: React.SetStateAction<string> } }) => {
-    setValue(event.target.value);
-  };
-
-  const addTodoHandler = (content: string) => {
-    const newTodoArray: Todo[] = todos;
-    newTodoArray.push({ content });
+  const addTodo = (todo: Todo) => {
+    const newTodoArray: Todo[] = [...todos];
+    newTodoArray.push(todo);
     setTodos(newTodoArray);
-    setValue('');
   };
 
   return (
     <Flex styles={styles.rootContainer}>
       <Flex column styles={styles.contentContainer}>
         <Flex wrap>
-          {todos ? (
+          {todos.length ? (
             todos.map((todo: Todo) => {
               return <TodoCard content={todo.content} />;
             })
@@ -35,12 +29,7 @@ const TodoBoard: React.FC = (props) => {
             <Text content={'There are no todos yet!'} size={'24px'} />
           )}
         </Flex>
-        <Flex column>
-          <TextField label='Todo content' multiline maxRows={4} value={value} onChange={handleChange} />
-          <Button variant='outlined' onClick={() => addTodoHandler(value)}>
-            Add
-          </Button>
-        </Flex>
+        <AddTodoForm addTodo={addTodo} />
       </Flex>
     </Flex>
   );
