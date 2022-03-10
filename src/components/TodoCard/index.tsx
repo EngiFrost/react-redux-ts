97e85a5f9f117818388c3a4d6e192b22635eac38
Daniel Frost <flint.bets@yandex.ru>
2022-03-10 21:37:00 +0300

    Created styles for todo card

--- src/components/TodoCard/index.tsx
+++ src/components/TodoCard/index.tsx
@@ -1,15 +1,21 @@
 import { Card } from '@mui/material';
 import Text from '../layout/Text';
 import React from 'react';
+import todoCardStyles from './styles';
 
 interface ITodoProps {
   content: string;
 }
 
 const TodoCard: React.FC<ITodoProps> = (props) => {
+  const styles = todoCardStyles;
   const { content } = props;
 
-  return <Card children={<Text content={content} />} />;
+  return (
+    <Card style={styles.card}>
+      <Text align='center' content={content} fluid styles={styles.cardContent} />
+    </Card>
+  );
 };
 
 export default TodoCard;
---
+++ src/components/TodoCard/styles.tsx
@@ -0,0 +1,14 @@
+const todoCardStyles = {
+  card: {
+    height: '100px',
+    width: '250px',
+    display: 'flex',
+    alignItems: 'center',
+  },
+  cardContent: {
+    lineHeight: '18px',
+    fontSize: '18px',
+  },
+};
+
+export default todoCardStyles;
