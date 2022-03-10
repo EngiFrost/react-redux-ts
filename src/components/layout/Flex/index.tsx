363d42249553fe08a5c1ce01b65a2d034882e7f3
Daniel Frost <flint.bets@yandex.ru>
2022-03-10 21:14:13 +0300

    Added fill prop

--- src/components/layout/Flex/index.tsx
+++ src/components/layout/Flex/index.tsx
@@ -11,20 +11,23 @@ export enum GapTypes {
 interface IFlexProps {
   children: React.ReactNode;
   column?: boolean;
+  fill?: boolean;
   gap?: GapTypes;
   styles?: React.CSSProperties;
   wrap?: boolean;
 }
 
 const Flex: React.FC<IFlexProps> = (props) => {
-  const { children, column = false, gap = GapTypes.NONE, styles = {}, wrap = false } = props;
+  const { children, column = false, fill = false, gap = GapTypes.NONE, styles, wrap = false } = props;
 
   const style: React.CSSProperties = {
-    ...styles,
     display: 'flex',
     flexDirection: column ? 'column' : 'row',
     flexWrap: wrap ? 'wrap' : 'nowrap',
     gap: `${Math.pow(4, gap)}px`,
+    height: fill ? '100%' : 'fit-content',
+    width: fill ? '100%' : 'fit-content',
+    ...styles,
   };
 
   return <div style={style}>{children}</div>;
