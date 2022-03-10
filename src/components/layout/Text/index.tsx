b26044927df9b2f926fcb140c589ee6f98ceee38
Daniel Frost <flint.bets@yandex.ru>
2022-03-10 21:36:09 +0300

    Added align prop

--- src/components/layout/Text/index.tsx
+++ src/components/layout/Text/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 interface ITextProps {
+  align?: 'left' | 'center' | 'right';
   content: string;
   fluid?: boolean;
   size?: string;
@@ -8,10 +9,11 @@ interface ITextProps {
 }
 
 const Text: React.FC<ITextProps> = (props) => {
-  const { content, fluid = false, size, styles } = props;
+  const { align = 'left', content, fluid = false, size, styles } = props;
 
   const style: React.CSSProperties = {
     fontSize: size,
+    textAlign: align,
     width: fluid ? '100%' : 'fit-content',
     ...styles,
   };
