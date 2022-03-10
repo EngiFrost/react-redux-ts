00916b9c027fd1b20de38c3b7a9e5d147e8d5d61
Daniel Frost <flint.bets@yandex.ru>
2022-03-10 21:14:41 +0300

    Improved Text component

--- src/components/layout/Text/index.tsx
+++ src/components/layout/Text/index.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
 
 interface ITextProps {
-  content: string
-  size?: string
+  content: string;
+  size?: string;
+  styles?: React.CSSProperties;
 }
 
 const Text: React.FC<ITextProps> = (props) => {
-  const {content, size} = props
+  const { content, size, styles } = props;
 
   const style: React.CSSProperties = {
-    fontSize: size
-  }
+    fontSize: size,
+    ...styles,
+  };
 
-  return <p style={style}>{content}</p>
-}
+  return <p style={style}>{content}</p>;
+};
 
 export default Text
