5d12bc9097fde00965b7747809337c56bfb64028
Daniel Frost <flint.bets@yandex.ru>
2022-03-10 21:25:55 +0300

    Improved Text component

--- src/components/layout/Text/index.tsx
+++ src/components/layout/Text/index.tsx
@@ -2,15 +2,17 @@ import React from 'react';
 
 interface ITextProps {
   content: string;
+  fluid?: boolean;
   size?: string;
   styles?: React.CSSProperties;
 }
 
 const Text: React.FC<ITextProps> = (props) => {
-  const { content, size, styles } = props;
+  const { content, fluid = false, size, styles } = props;
 
   const style: React.CSSProperties = {
     fontSize: size,
+    width: fluid ? '100%' : 'fit-content',
     ...styles,
   };
 
