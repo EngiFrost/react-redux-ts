77e047e8d48b4fee29fd6b606c8457fc0c141dea
Daniel Frost <flint.bets@yandex.ru>
2022-03-19 18:48:53 +0300

    Created typed selector hook & connected state

--- src/components/views/TodoView/index.tsx
+++ src/components/views/TodoView/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useTypedSelector } from '../../../hooks/useTypedSelector';
 import { Todo } from '../../../models/Todo';
 import AddTodoForm from '../../AddTodoForm';
 import Flex from '../../layout/Flex';
@@ -7,6 +8,8 @@ import todoViewStyles from './styles';
 
 const TodoView: React.FC = (props) => {
   const styles = todoViewStyles;
+  const state = useTypedSelector((state) => state.todo);
+  console.log(state);
 
   const [todos, setTodos] = useState<Todo[]>([]);
 
@@ -24,7 +27,7 @@ const TodoView: React.FC = (props) => {
   return (
     <Flex fill hAlign='center' vAlign='center' styles={styles.rootContainer}>
       <Flex fill column gap='gap.medium' hAlign='center' styles={styles.contentContainer}>
-        <TodoBoard todos={todos} />
+        <TodoBoard todos={state.todos} />
         <AddTodoForm addTodo={addTodo} styles={styles.form} />
       </Flex>
     </Flex>
--- src/store/index.ts
+++ src/store/index.ts
@@ -2,3 +2,5 @@ import { applyMiddleware, createStore } from 'redux'
 import { rootReducer } from './reducers'
 
 export const store = createStore(rootReducer, applyMiddleware())
+
+export type RootState = ReturnType<typeof rootReducer>
