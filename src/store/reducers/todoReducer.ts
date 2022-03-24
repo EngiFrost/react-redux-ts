4e484dd155111c4a9ee638ccd5c2b844798d8955
Daniel Frost <flint.bets@yandex.ru>
2022-03-24 20:00:56 +0300

    Handled new case in reducer

--- src/store/reducers/todoReducer.ts
+++ src/store/reducers/todoReducer.ts
@@ -8,9 +8,11 @@ const initialState: TodoState = {
 export const todoReducer = (state: TodoState = initialState, action: TodoAction): TodoState => {
   switch (action.type) {
     case TodoActionTypes.ADD_TODO:
-      return { ...state, ...action.payload };
+      return { ...state, todos: [...state.todos, action.payload as Todo] };
     case TodoActionTypes.REMOVE_TODO:
-      return { ...state, todos: state.todos.filter((todo) => todo.id !== action.payload) }
+      return { ...state, todos: state.todos.filter((todo) => todo.id !== action.payload as string) }
+    case TodoActionTypes.FETCH_TODOS:
+      return { ...state, todos: [...state.todos, ...action.payload as Todo[]] }
     default:
       return state
   }
